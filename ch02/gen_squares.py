335c826f277b09f0c0961fb4b14b96f78c82f908
John Bobo <john.bobo@ltxsolutions.com>
2019-06-17 10:44:44 -0400

    rewrote gen_squares

--- ch02/gen_squares.py
+++ ch02/gen_squares.py
@@ -1,8 +1,7 @@
-def gen_squares(max_num):
-    for num in range(max_num):
-        yield num**2
+def gen_squares(max_root):
+    for n in range(max_root):
+        yield n**2
 
 MAX = 5
-
 for square in gen_squares(MAX):
-    print(square)
+    print square
